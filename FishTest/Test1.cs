c82de3c37fe108b8621786d656bc5fc07a278bd7
EKfleurs <ST10439486@vcconnect.edu.za>
2025-08-01 14:44:23 +0200

    Add project files.

---
+++ FishTest/Test1.cs
@@ -0,0 +1,38 @@
+﻿using IndexICE;
+
+namespace FishTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Fish fish = new Fish(20, 3, "Summer", "Shad");
+            Assert.AreEqual(fish[3], "Shad");
+        }
+
+        [TestMethod]
+        public void updateWeightTest()
+        {
+            Fish fish = new Fish(20, 3, "Summer", "Shad");
+            fish[1] = 12; //int index
+            Assert.AreEqual(fish[1], 12);
+        }
+
+        [TestMethod]
+        public void testSpecies()
+        {
+            Fish fish = new Fish(20, 3, "Summer", "Shad");
+            Assert.AreEqual(fish["fishSpecies"], "Shad");//string index
+        }
+
+        [TestMethod]
+        public void updateSeason()
+        {
+            Fish fish = new Fish(6, 10, "Summer", "Shad");
+            fish["Season"] = 7; //int index
+            Assert.AreEqual(fish["Season"], 7);
+        }
+    }
+}
---
+++ IndexICE/Fish.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexICE
+{
+    public class Fish
+    {
+        int lenght, fishWeight;
+        string Season, fishSpecies;
+
+        public Fish(int lenght, int fishWeight, string Season, string fishSpecies)
+        {
+            this.lenght = lenght;
+            this.fishWeight = fishWeight;
+            this.Season = Season;
+            this.fishSpecies = fishSpecies;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return this.lenght;
+                else if (index == 1)
+                    return this.fishWeight;
+                else if (index == 2)
+                    return this.Season;
+                else if (index == 3)
+                    return this.fishSpecies;
+                return null;
+            }
+            set
+            {
+                if (index == 0)
+                    this.lenght = (int)value;
+                else if (index == 1)
+                    this.fishWeight = (int)value;
+                else if (index == 2)
+                    this.Season = (string)value;
+                else if (index == 3)
+                    this.fishSpecies = (string)value;
+            }
+        }
+
+        public object this[string attrName]
+        {
+            get
+            {
+                if (attrName == "lenght")
+                    return this.lenght;
+                else if (attrName == "fishWheight")
+                    return this.fishWeight;
+                else if (attrName == "season")
+                    return this.Season;
+                else if (attrName == "fishSpecies")
+                    return this.fishSpecies;
+                return null;
+            }
+            set
+            {
+                if (attrName.ToLower().Equals("lenght"))
+                    this.lenght = (int)value;
+                else if (attrName.ToLower().Equals("weight"))
+                    this.fishWeight = (int)value;
+                else if (attrName.ToLower().Equals("season"))
+                    this.Season = (string)value;
+                else if (attrName.ToLower().Equals("fishSpecies"))
+                    this.fishSpecies = (string)value;
+            }
+        }
+    } 
+}
